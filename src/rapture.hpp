db58a8b3fbec1b24262614761e31021455e602d2
Christopher Kaster <me@atomicptr.de>
2024-08-31 10:43:52 +0200

    prefix code loc var to avoid potential shadowing

--- src/rapture.hpp
+++ src/rapture.hpp
@@ -4,24 +4,31 @@
 #include <print>
 #include <source_location>
 
-#define assert(condition, ...)                                                                                      \
-    if (!(condition)) {                                                                                             \
-        auto loc = std::source_location::current();                                                                 \
-        std::println("");                                                                                           \
-        std::println("========= ASSERTATION FAILED {}:{}:{} =========", loc.file_name(), loc.line(), loc.column()); \
-        std::println("\tAssert :\t{}", (#condition));                                                               \
+#define assert(condition, ...)                                    \
+    if (!(condition)) {                                           \
+        auto rapture_loc = std::source_location::current();       \
+        std::println("");                                         \
+        std::println(                                             \
+            "========= ASSERTATION FAILED {}:{}:{} =========",    \
+            rapture_loc.file_name(),                              \
+            rapture_loc.line(),                                   \
+            rapture_loc.column()                                  \
+        );                                                        \
+        std::println("\tAssert :\t{}", (#condition));             \
+        std::println("\tMessage:\t{}", std::format(__VA_ARGS__)); \
+        std::println("");                                         \
+        std::abort();                                             \
+    }
+
+#define panic(...)                                                                                                  \
+    {                                                                                                               \
+        auto rapture_loc = std::source_location::current();                                                         \
+        std::println(                                                                                               \
+            "========= PANIC {}:{}:{} =========", rapture_loc.file_name(), rapture_loc.line(), rapture_loc.column() \
+        );                                                                                                          \
         std::println("\tMessage:\t{}", std::format(__VA_ARGS__));                                                   \
         std::println("");                                                                                           \
         std::abort();                                                                                               \
-    }
-
-#define panic(...)                                                                                     \
-    {                                                                                                  \
-        auto loc = std::source_location::current();                                                    \
-        std::println("========= PANIC {}:{}:{} =========", loc.file_name(), loc.line(), loc.column()); \
-        std::println("\tMessage:\t{}", std::format(__VA_ARGS__));                                      \
-        std::println("");                                                                              \
-        std::abort();                                                                                  \
     }
 
 #define unreachable() panic("Unreachable code executed!")

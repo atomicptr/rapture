1cb0443b75417a7e4649c5af1e3ca2cc396b7664
Christopher Kaster <me@atomicptr.de>
2024-09-02 11:00:37 +0200

    print to stderr instead of stdout

--- src/rapture.hpp
+++ src/rapture.hpp
@@ -11,20 +11,21 @@
     #undef assert
 #endif
 
-#define assert(condition, ...)                                    \
-    if (!(condition)) {                                           \
-        auto rapture_loc = std::source_location::current();       \
-        std::println("");                                         \
-        std::println(                                             \
-            "========= ASSERTATION FAILED {}:{}:{} =========",    \
-            rapture_loc.file_name(),                              \
-            rapture_loc.line(),                                   \
-            rapture_loc.column()                                  \
-        );                                                        \
-        std::println("\tAssert :\t{}", (#condition));             \
-        std::println("\tMessage:\t{}", std::format(__VA_ARGS__)); \
-        std::println("");                                         \
-        std::abort();                                             \
+#define assert(condition, ...)                                            \
+    if (!(condition)) {                                                   \
+        auto rapture_loc = std::source_location::current();               \
+        std::println(stderr, "");                                         \
+        std::println(                                                     \
+            stderr,                                                       \
+            "========= ASSERTATION FAILED {}:{}:{} =========",            \
+            rapture_loc.file_name(),                                      \
+            rapture_loc.line(),                                           \
+            rapture_loc.column()                                          \
+        );                                                                \
+        std::println(stderr, "\tAssert :\t{}", (#condition));             \
+        std::println(stderr, "\tMessage:\t{}", std::format(__VA_ARGS__)); \
+        std::println(stderr, "");                                         \
+        std::abort();                                                     \
     }
 
 #ifdef panic
@@ -34,15 +35,19 @@
     #undef panic
 #endif
 
-#define panic(...)                                                                                                  \
-    {                                                                                                               \
-        auto rapture_loc = std::source_location::current();                                                         \
-        std::println(                                                                                               \
-            "========= PANIC {}:{}:{} =========", rapture_loc.file_name(), rapture_loc.line(), rapture_loc.column() \
-        );                                                                                                          \
-        std::println("\tMessage:\t{}", std::format(__VA_ARGS__));                                                   \
-        std::println("");                                                                                           \
-        std::abort();                                                                                               \
+#define panic(...)                                                        \
+    {                                                                     \
+        auto rapture_loc = std::source_location::current();               \
+        std::println(                                                     \
+            stderr,                                                       \
+            "========= PANIC {}:{}:{} =========",                         \
+            rapture_loc.file_name(),                                      \
+            rapture_loc.line(),                                           \
+            rapture_loc.column()                                          \
+        );                                                                \
+        std::println(stderr, "\tMessage:\t{}", std::format(__VA_ARGS__)); \
+        std::println(stderr, "");                                         \
+        std::abort();                                                     \
     }
 
 #ifdef unreachable
@@ -52,18 +57,19 @@
     #undef unreachable
 #endif
 
-#define unreachable()                                                                                 \
-    {                                                                                                 \
-        auto rapture_loc = std::source_location::current();                                           \
-        std::println(                                                                                 \
-            "========= UNREACHABLE CODE {}:{}:{} =========",                                          \
-            rapture_loc.file_name(),                                                                  \
-            rapture_loc.line(),                                                                       \
-            rapture_loc.column()                                                                      \
-        );                                                                                            \
-        std::println("\tMessage:\tThis point was marked as unreachable, but was reached regardless"); \
-        std::println("");                                                                             \
-        std::abort();                                                                                 \
+#define unreachable()                                                                                         \
+    {                                                                                                         \
+        auto rapture_loc = std::source_location::current();                                                   \
+        std::println(                                                                                         \
+            stderr,                                                                                           \
+            "========= UNREACHABLE CODE {}:{}:{} =========",                                                  \
+            rapture_loc.file_name(),                                                                          \
+            rapture_loc.line(),                                                                               \
+            rapture_loc.column()                                                                              \
+        );                                                                                                    \
+        std::println(stderr, "\tMessage:\tThis point was marked as unreachable, but was reached regardless"); \
+        std::println(stderr, "");                                                                             \
+        std::abort();                                                                                         \
     }
 
 #ifdef unimplemented
@@ -73,16 +79,17 @@
     #undef unimplemented
 #endif
 
-#define unimplemented()                                                   \
-    {                                                                     \
-        auto rapture_loc = std::source_location::current();               \
-        std::println(                                                     \
-            "========= UNIMPLEMENTED CODE {}:{}:{} =========",            \
-            rapture_loc.file_name(),                                      \
-            rapture_loc.line(),                                           \
-            rapture_loc.column()                                          \
-        );                                                                \
-        std::println("\tMessage:\tThis feature is not yet implemented."); \
-        std::println("");                                                 \
-        std::abort();                                                     \
+#define unimplemented()                                                           \
+    {                                                                             \
+        auto rapture_loc = std::source_location::current();                       \
+        std::println(                                                             \
+            stderr,                                                               \
+            "========= UNIMPLEMENTED CODE {}:{}:{} =========",                    \
+            rapture_loc.file_name(),                                              \
+            rapture_loc.line(),                                                   \
+            rapture_loc.column()                                                  \
+        );                                                                        \
+        std::println(stderr, "\tMessage:\tThis feature is not yet implemented."); \
+        std::println(stderr, "");                                                 \
+        std::abort();                                                             \
     }
